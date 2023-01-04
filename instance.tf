b03477dd2e6698037b362a226032d16f6e75c29b
omarsharif22 <109093557+omarsharif22@users.noreply.github.com>
2023-01-04 12:07:40 +0000

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,26 @@
+#Create ec2
+resource "aws_instance" "BabyGaffServer" {
+  ami           = "DUMMY_VALUE_AMI_ID"
+  subnet_id     = "BabyGaffPublic1a"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = BabyGaffSG
+  
+  tags = {
+    Name = "BabyGaff Web Server"
+  }
+}
+
+#Create key pair
+resource "tls_private_key" "BabyGaffKP" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "BabyGaffKP" {
+  key_name   = "BabyGaffKP"
+  public_key = tls_private_key.BabyGaffKP.public_key_openssh
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.BabyGaffKP.private_key_pem}' > ./Downloads/BabyGaffKP.pem"
+  }
+}
