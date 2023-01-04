4a9f79ac8a4c0821dbf293e0a48156cf5df84ce9
omarsharif22 <109093557+omarsharif22@users.noreply.github.com>
2023-01-04 23:06:13 +0000

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "BabyGaffPUB" {
   instance_type = "t2.micro"
   key_name      = aws_key_pair.key121.key_name
   subnet_id     = aws_subnet.BabyGaffPublic1a.id
-  vpc_security_group_ids = data.aws_security_group.BabyGaffSG.id
+  vpc_security_group_ids = ["aws_security_group.BabyGaffSG.id"]
   
   tags = {
     name = "PUB-BabyGaffServer"
@@ -32,7 +32,7 @@ resource "aws_instance" "BabyGaffPRIV" {
   instance_type = "t2.micro"
   key_name      = aws_key_pair.key121.key_name
   subnet_id     = aws_subnet.BabyGaffPrivate1a.id
-  vpc_security_group_ids = data.aws_security_group.BabyGaffSG.id
+  vpc_security_group_ids = ["aws_security_group.BabyGaffSG.id"]
   
   tags = {
     name = "PRIV-BabyGaffServer"
